b7f3b307b8dbefbf1c2e9a208a891e8a893915ad
Felipe Figueiredo <recoverxfj@gmail.com>
2025-09-20 17:06:48 -0300

    Redirect a Magento controller action with a factory

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -4,11 +4,17 @@ declare(strict_types=1);
 namespace Chuazinerd\Blog\Controller\Index;
 
 use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\Controller\Result\RedirectFactory;
 
 class Index implements HttpGetActionInterface
 {
-    public function execute()
+    public function __construct(
+        private RedirectFactory $redirectFactory,
+    ) {}
+
+    public function execute(): Redirect
     {
-        die('Die blog');
+        $redirect = $this->redirectFactory->create();
+        return $redirect->setPath('blog/post/list');
     }
 }
