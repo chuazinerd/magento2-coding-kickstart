c2f62c080a042b4af1f113a6f7f0ca56c334aac6
Felipe Figueiredo <recoverxfj@gmail.com>
2025-09-29 07:45:11 -0300

    Create a repository

--- Api/PostRepositoryInterface.php
+++ Api/PostRepositoryInterface.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Macademy\Blog\Api;
+namespace Chuazinerd\Blog\Api;
 
 use Macademy\Blog\Api\Data\PostInterface;
 use Magento\Framework\Exception\LocalizedException;
