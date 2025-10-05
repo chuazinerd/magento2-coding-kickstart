9946d98f75d0a9e95ef026d1a4fbd007b8d8831c
Felipe Figueiredo <recoverxfj@gmail.com>
2025-10-05 15:27:52 -0300

    Create and dispatch an event

--- Controller/Post/Detail.php
+++ Controller/Post/Detail.php
@@ -2,8 +2,8 @@
 
 namespace Chuazinerd\Blog\Controller\Post;
 
-
 use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\Event\ManagerInterface as EventManager;
 use Magento\Framework\View\Result\Page;
 use Magento\Framework\View\Result\PageFactory;
 
@@ -13,10 +13,15 @@ class Detail implements HttpGetActionInterface
 
     public function __construct(
         private PageFactory $pageFactory,
+        private EventManager $eventManager,
+        private RequestInterface $request,
     ) {}
 
     public function execute() : Page
     {
-        return $this->pageFactory->create();
+        $this->eventManager->dispatch('macademy_blog_post_detail_view', [
+            'request' => $this->request,
+        ]);
+    return $this->pageFactory->create();
     }
 }
